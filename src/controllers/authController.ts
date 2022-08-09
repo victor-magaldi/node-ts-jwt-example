2712aa42f7261667278f7806d6874ccd108c7cc5
victor de souza magaldi <victmagaldi@hotmail.com>
2022-08-09 19:46:34 -0300

    message user

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -2,11 +2,10 @@ import { Request, Response } from 'express'
 
 export const registerUser = (req: Request, res: Response) => {
   const { name, email, password, confirmPassword } = req.body
-
   if (!name || !email || !password || !confirmPassword) {
-    return res
-      .status(422)
-      .json({ error: 'name, email, password, confirmPassword  required' })
+    return res.status(422).json({
+      error: 'name, email, password, confirmPassword are required fields',
+    })
   }
 
   res.json({ ok: true })
