0ea76759f1fa286e9648869f2a4547070fdb3514
victor de souza magaldi <victmagaldi@hotmail.com>
2022-08-09 20:13:50 -0300

    init bcrypt

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from 'express'
+import bcrypt from 'bcrypt'
 
-export const registerUser = (req: Request, res: Response) => {
+export const registerUser = async (req: Request, res: Response) => {
   const { name, email, password, confirmPassword } = req.body
   if (!name || !email || !password || !confirmPassword) {
     return res.status(422).json({
@@ -15,5 +16,16 @@ export const registerUser = (req: Request, res: Response) => {
       error: 'E-mail já cadasttrado',
     })
   }
-  res.json({ ok: true })
+  //created password
+  try {
+    const salt = await bcrypt.genSalt(12)
+    const passwordHash = await bcrypt.hash(password, salt)
+
+    console.log({ name, email, password })
+    console.log(passwordHash)
+    res.json({ ok: true })
+  } catch (error) {
+    console.log('error ', error)
+    res.status(500).json({ error })
+  }
 }
