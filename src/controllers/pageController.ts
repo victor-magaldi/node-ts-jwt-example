13deab1d19ddb1b08d2a912d2bc141bfb46d2d50
victor de souza magaldi <victmagaldi@hotmail.com>
2022-08-31 19:54:57 -0300

    init save key

--- src/controllers/pageController.ts
+++ src/controllers/pageController.ts
@@ -1,6 +1,17 @@
 import { Request, Response } from 'express'
+import { createClient } from 'redis'
 
 export const home = (req: Request, res: Response) => {
-  console.log('teste')
+  const client = createClient()
+  client.on('error', (err) => console.log('Redis Client Error', err))
+  async function getData() {
+    await client.connect()
+
+    await client.set('victor', 'teste')
+    const value = await client.get('victor')
+
+    console.log(value)
+  }
+  getData()
   res.json({ ok: true })
 }
