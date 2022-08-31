71c5b81bcffea82f805461f3860b1244616d1199
victor de souza magaldi <victmagaldi@hotmail.com>
2022-08-31 19:46:53 -0300

    install redis

--- src/controllers/pageController.ts
+++ src/controllers/pageController.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from 'express'
 
 export const home = (req: Request, res: Response) => {
+  console.log('teste')
   res.json({ ok: true })
 }
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,27 @@
 import { Router } from 'express'
 import * as PageController from '../controllers/pageController'
 import * as AuthController from '../controllers/authController'
+import { json } from 'stream/consumers'
 
 const router = Router()
 
 router.get('/', PageController.home)
 router.post('/auth/register', AuthController.registerUser)
+router.get(
+  '/ping',
+  function (req, res, next) {
+    const { name, age } = req.query
+    console.log('name:', name, 'age', age)
+    next()
+  },
+  function (req, res, next) {
+    console.log('baseUrl 2:', req.ip)
+    next()
+  },
+  function (req, res, next) {
+    const { name, age } = req.query
+    res.json({ name, age })
+  }
+)
 
 export { router }
