92ffb0ca77f87ff239a041eb3ed5a990c3482faf
victor de souza magaldi <victmagaldi@hotmail.com>
2023-01-07 21:26:23 -0300

    remove config ejs

--- src/server.ts
+++ src/server.ts
@@ -7,11 +7,9 @@ dotenv.config()
 
 const app = express()
 
-// config json
+// config json response
 app.use(express.json())
 
-app.set('view engine', 'ejs')
-app.set('views', path.join(__dirname, 'views'))
 app.use(express.static(path.join(__dirname, '../public')))
 
 app.use(router)
